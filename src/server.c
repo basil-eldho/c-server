5ac216c6afee2b5d00bf59cce60a79b9fb3c7888
Basil Eldho <basileljol@gmail.com>
2026-04-01 21:23:17 -0400

    Phase 2+3: HTTP request parsing and response

--- src/server.c
+++ src/server.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <unistd.h>
@@ -6,14 +7,66 @@
 #define PORT 3000
 #define BUFFER_SIZE 4096
 
-int main() {
-    int server_fd;
-    int client_fd;
+struct header {
+    char *name;
+    char *value;
+};
 
+struct request {
+    char *method;
+    char *path;
+    int protocol;
+    int nheaders;
+    struct header headers[32];
+};
+
+int parse_request(char *buf, int n, struct request *req) {
+    req->method = buf;
+
+    char *sp = memchr(buf, ' ', n);
+    if (sp == NULL)
+        return -1;
+    *sp = '\0';
+    req->path = sp + 1;
+
+    sp = memchr(req->path, ' ', n - (req->path - buf));
+    if (sp == NULL)
+        return -1;
+    *sp = '\0';
+
+    char *ver = sp + 1;
+
+    if (ver[5] == '1' && ver[7] == '0')
+        req->protocol = 10;
+    else if (ver[5] == '1' && ver[7] == '1')
+        req->protocol = 11;
+    else
+        return -1; // unsupported
+
+    return 0;
+}
+
+int send_response(int fd, int status_code, char *body) {
+    char buf[BUFFER_SIZE];
+    int len = snprintf(buf, sizeof(buf),
+                       "HTTP/1.1 %d OK\r\n"
+                       "Content-Length: %d\r\n"
+                       "\r\n"
+                       "%s",
+                       status_code, (int) strlen(body), body);
+
+    if (write(fd, buf, len) == -1) {
+        perror("send response: write");
+        return 1;
+    }
+    return 0;
+}
+
+int main() {
     struct sockaddr_in addr;
     socklen_t addrlen = sizeof(addr);
 
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd == -1) {
         perror("socket");
         return 1;
@@ -22,6 +75,9 @@ int main() {
     addr.sin_family = AF_INET;
     addr.sin_port = htons(PORT);
     addr.sin_addr.s_addr = INADDR_ANY;
+
+    int opt = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
     if (bind(server_fd, (struct sockaddr *) &addr, addrlen) == -1) {
         perror("bind");
@@ -32,31 +88,36 @@ int main() {
         perror("listen");
         return 1;
     }
+    while (1) {
+        int client_fd = accept(server_fd, (struct sockaddr *) &addr, &addrlen);
+        if (client_fd == -1) {
+            perror("accept");
+            return 1;
+        }
 
-    client_fd = accept(server_fd, (struct sockaddr *) &addr, &addrlen);
-    if (client_fd == -1) {
-        perror("accept");
-        return 1;
+        printf("client: %s:%d\n\n", inet_ntoa(addr.sin_addr), ntohs(addr.sin_port));
+        fflush(stdout);
+
+        char buf[BUFFER_SIZE];
+        int n = read(client_fd, buf, sizeof(buf));
+        if (n == -1) {
+            perror("read");
+            return 1;
+        }
+
+        struct request req;
+        if (parse_request(buf, n, &req) == -1) {
+            fprintf(stderr, "parse request\n");
+            close(client_fd);
+            continue;
+        }
+
+        printf("parsed request: %s, %s, %d\n", req.method, req.path, req.protocol);
+
+        send_response(client_fd, 200, "Hello world\n");
+
+        close(client_fd);
     }
-
-    printf("Connection established!!!\n");
-
-    char buf[BUFFER_SIZE];
-    int n = read(client_fd, buf, sizeof(buf));
-    if (n == -1) {
-        perror("read");
-        return 1;
-    }
-
-    printf("received: %.*s\n", n, buf);
-    fflush(stdout);
-
-    if (write(client_fd, buf, n) == -1) {
-        perror("write");
-        return 1;
-    }
-
-    close(client_fd);
     close(server_fd);
 
     return 0;
